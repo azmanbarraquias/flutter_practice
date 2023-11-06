914d2160eba398ee51290ba9107633e4fdf1818f
Azman Barraquias <azmanbarraquias@gmail.com>
2023-11-07 01:33:33 +0800

    Create main_rest_api.dart

---
+++ practice_flutter_app/lib/restapi_flutter/main_rest_api.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const RestAPISample());
+
+class RestAPISample extends StatelessWidget {
+  const RestAPISample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
